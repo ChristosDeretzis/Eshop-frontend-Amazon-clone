896f46a2bb7f9c59001f17f271349edf62f1437e
Christos Deretzis <deretzischristos@gmail.com>
2020-12-05 00:54:49 +0200

    Add main header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Login from "./components/Login/Login";
 import SignUp from "./components/SignUp/SignUp";
+import Header from "./components/Header/Header";
 import './App.css';
 
 function App() {
@@ -13,6 +14,9 @@ function App() {
           </Route>
           <Route path="/signup">
             <SignUp />
+          </Route>
+          <Route path="/">
+            <Header />
           </Route>
         </Switch>
      </div>
